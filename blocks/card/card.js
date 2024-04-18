b9c3d5f56e35b51e6de172c78705b99fb057607e
Johan Trieloff <johan@trieloff.net>
2024-04-18 16:59:57 +0200

    Styled heading

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -1,3 +1,13 @@
-export default function decorate() {
-  console.log('decorate card');
+export default function decorate(block) {
+  // Find the <strong> element inside the specified <p> tag
+  const strong = block.querySelector('.card div div:nth-child(2) p:nth-child(2) strong');
+
+  // If the <strong> element is found, replace the <p> tag with an <h1> tag
+  if (strong) {
+    const h1 = document.createElement('h1');
+    h1.textContent = strong.textContent;
+    const parentP = strong.parentElement;
+
+    parentP.parentNode.replaceChild(h1, parentP);
+  }
 }
